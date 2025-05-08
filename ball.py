3ca7f93f3706b58bb70f607905e7397b3a825f43
YangTh599 <YangTh599@northwested.org>
2025-05-08 11:38:09 -0400

    b

--- ball.py
+++ ball.py
@@ -7,22 +7,22 @@ class Ball(shapes.Circ):
     def __init__(self, window, color, center_coords, radius, width=0):
         super().__init__(window,color,center_coords, radius, width)\
         
-        self.x_vel = r(1,10)
-        self.y_vel = r(1,10)
+        self.x_vel = r(3,10)
+        self.y_vel = r(3,10)
 
     def check_collisons(self):
         if self.x - self.r < 0:
             self.change_x_pos(self.r)
-            self.x_vel = r(1,10)
+            self.x_vel = r(3,10)
         if self.x + self.r > cfig.SCREEN_WIDTH:
             self.change_x_pos(cfig.SCREEN_WIDTH - self.r)
-            self.x_vel = r(-10,-1)
+            self.x_vel = r(-10,-3)
         if self.y - self.r < 0:
             self.change_y_pos(self.r)
-            self.y_vel = r(1,10)
+            self.y_vel = r(3,10)
         if self.y + self.r > cfig.SCREEN_HEIGHT:
             self.change_y_pos(cfig.SCREEN_HEIGHT - self.r)
-            self.y_vel = r(-10,-1)
+            self.y_vel = r(-10,-3)
 
     def move(self):
         self.change_pos(self.x + self.x_vel, self.y + self.y_vel)
